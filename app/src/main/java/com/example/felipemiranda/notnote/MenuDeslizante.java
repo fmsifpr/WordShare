3fb87792be4e248344e8b9dbd417e7a2d55cf86a
fmsifpr <fmsifpr@gmail.com>
2019-04-09 11:52:17 -0300

    Toast, classes novas, criarnota

--- app/src/main/java/com/example/felipemiranda/notnote/MenuDeslizante.java
+++ app/src/main/java/com/example/felipemiranda/notnote/MenuDeslizante.java
@@ -68,7 +68,7 @@ public class MenuDeslizante extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_configurar) {
             return true;
         }
 
