e1d21f293e1f9381682a64969b457d3b135c5bed
fmsifpr <fmsifpr@gmail.com>
2019-04-16 11:26:11 -0300

    varias coisas

---
+++ app/src/main/java/com/example/felipemiranda/notnote/Notas.java
@@ -0,0 +1,13 @@
+package com.example.felipemiranda.notnote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Notas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas);
+    }
+}
